50b394eace0b5d2473be9930c7b7822e04dedc2c
Yor <yorcee@outlook.com>
2016-11-14 17:32:57 +0800

    登陆
    登陆界面

---
+++ pages/user/user.js
@@ -0,0 +1,27 @@
+var app = getApp();
+Page({
+  data:{
+   
+  },
+  onLoad:function(options){
+    if(app.appData.userinfo == null ){
+          wx.redirectTo({url:"../login/login"})
+    }
+  },
+  onReady:function(){
+    // 页面渲染完成
+
+  },
+  onShow:function(){
+    // 页面显示
+ 
+  },
+  onHide:function(){
+    // 页面隐藏
+
+  },
+  onUnload:function(){
+    // 页面关闭
+  
+  }
+})
